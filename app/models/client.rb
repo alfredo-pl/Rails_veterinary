5a162d95c91b885057ecb4c6dcd6eb1e5df90bc9
alfredopl <aalfredoplz@gmail.com>
2021-01-26 20:38:27 -0300

    methods instance client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,28 @@
 class Client < ApplicationRecord
     has_many :pets , dependent: :destroy
+
+    def get_pets
+        Pet.references(:clients).where(client_id: id).count
+    end
+    def get_pet_name
+        Pet.references(:clients).where(client_id: id).pluck :name
+    end
+    def get_pet_max_w
+        no = "0"
+        max =   Pet.references(:clients).where(client_id: id).pluck :weight
+        if max.blank?
+            no
+        else
+            max.max
+        end
+    end
+    def get_pet_max_h
+        no = "0"
+        max =   Pet.references(:clients).where(client_id: id).pluck :height
+        if max.blank?
+            no
+        else
+            max.max
+        end
+    end
 end
