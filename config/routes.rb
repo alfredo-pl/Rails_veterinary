8987c26c57e9455a69d0f613680aa500b2545e4e
alfredopl <aalfredoplz@gmail.com>
2021-01-26 21:08:14 -0300

    root clients index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :pet_histories
   end
   resources :clients
+  root 'clients#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
